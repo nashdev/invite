eed5d9c861ba1ec336fda6a17905ad87c815ace1
William Golden <egdelwonk@gmail.com>
2019-01-06 12:00:17 -0600

    fix: update viewport for mobile

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="stylesheet"
             href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"
