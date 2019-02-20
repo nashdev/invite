6b397b2ca862978b920a3bf15054fb924c1b5f98
William Golden <egdelwonk@gmail.com>
2019-02-20 10:08:42 -0600

    chore: google analytics verification

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="google-site-verification" content="_X-CaJkIX0xzenu0IIHZlaCs1uJ6RhJCk-MuPGNE6ag" />
           <link
             rel="stylesheet"
             href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"
