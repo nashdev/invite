edbf7cf9944ef6e183fcc7e432a97537bc07166e
William Golden <egdelwonk@gmail.com>
2019-07-30 22:14:59 -0500

    chore: update document

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,26 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="google-site-verification" content="_X-CaJkIX0xzenu0IIHZlaCs1uJ6RhJCk-MuPGNE6ag" />
+          <meta
+            name="google-site-verification"
+            content="_X-CaJkIX0xzenu0IIHZlaCs1uJ6RhJCk-MuPGNE6ag"
+          />
           <link
             rel="stylesheet"
             href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"
           />
         </Head>
-        <body className="custom_class">
+        <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
